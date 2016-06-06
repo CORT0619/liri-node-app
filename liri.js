c4032ebcedb7193d0192792252da6510d17357c7
Crystal <cortney0215@yahoo.com>
2016-06-06 14:22:17 -0400

    adding all functions - need to complete spotify and last func

--- liri.js
+++ liri.js
@@ -1,8 +1,10 @@
+// imported keys & modules
 var keys = require('./keys.js');
-
-//console.log(keys.twitterKeys.consumer_key);
-
 var Twitter = require('twitter');
+var spotify = require('spotify');
+var request = require('request');
+var fs = require('fs');
+
 
 var client = new Twitter({
 
@@ -12,28 +14,37 @@ var client = new Twitter({
 	access_token_secret: keys.twitterKeys.access_token_secret
 });
 
-console.log("consumer key is: " + client.consumer_key);
+function runLiri(){
 
-var spotify = require('spotify');
-var request = require('request');
+	var action = process.argv[2];
+	var val = process.argv[3];
 
-var action = process.argv[2];
-var val = process.argv[3];
+	switch(action){
+		case 'my-tweets':
+			getTweets();
+		break;
 
-switch(action){
-	case 'my-tweets':
-		getTweets();
-	break;
+		case 'spotify-this-song':
+			getSpotified();
+		break;
 
-	case 'spotify-this-song':
-	break;
+		case 'movie-this':
+			switch(val){
+				case undefined:
+					getMovieStuff('Mr. Nobody');
+				break;
 
-	case 'movie-this':
-	break;
+				default:
+					getMovieStuff(val);
+			}
+			
+		break;
 
-	case 'do-what-it-says':
-	break;
+		case 'do-what-it-says':
+			doSomething();
+		break;
 
+	}
 }
 
 
@@ -56,8 +67,90 @@ function getTweets(){
 			}
 
 		}
-
-
 	});
 
 }
+
+function getSpotified(){
+
+	if(val != undefined){
+
+		spotify.search({type: 'track', query: val}, function(err, data){
+
+			if(err){
+
+				return console.log(err);
+			}
+
+			console.log(data);
+			console.log(data.tracks[0]);
+		});
+
+	} else {
+
+		spotify.search({type: 'track', query: 'what\'s my age again'}, function(err, data){
+
+			if(err){
+
+				return console.log(err);
+			}
+
+			console.log(data);
+		});
+	}
+}
+
+function getMovieStuff(movie){
+
+	if(process.argv.length > 4){
+
+		for(var i = 4; i < process.argv.length; i++){
+
+			movie += "+" +process.argv[i];
+
+		}
+	}
+
+	request('http://www.omdbapi.com/?t=' + movie + '&y=&plot=short&r=json&tomatoes=true', function(err, response, body){
+
+		if(err){
+
+			return console.log(err);
+		}
+
+		body = JSON.parse(body);
+
+		//console.log(body);
+		console.log("Movie title: " + body.Title);
+		console.log("Year: " + body.Year);
+		console.log("IMDB rating: " + body.imdbRating);
+		console.log("Country: " + body.Country);
+		console.log("Language: " + body.Language);
+		console.log("Plot: " + body.Plot);
+		console.log("Actors: " + body.Actors);
+		console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+		console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+	});
+
+}
+
+function doSomething(){
+
+	fs.readFile('./random.txt', "utf8", function(err, data){
+
+		if(err){
+			return console.log(err);
+		}
+
+		data = data.split(', ');
+
+		console.log(data[0]);
+
+
+
+
+
+	});
+}
+
+runLiri();
